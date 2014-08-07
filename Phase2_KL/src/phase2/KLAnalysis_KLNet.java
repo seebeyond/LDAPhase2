be843b30f98a109349230b5cd0a327c29dd934fe
ferrerooo <ferrerooo@gmail.com>
2014-08-07 11:08:24 -0400

    show all branch keywords

--- Phase2_KL/src/phase2/KLAnalysis_KLNet.java
+++ Phase2_KL/src/phase2/KLAnalysis_KLNet.java
@@ -5,25 +5,97 @@ import java.io.*;
 
 public class KLAnalysis_KLNet {
 	
-	public static double connectThreshold=3.2;
-	public static String outputPath = "C:/Users/zouc/Desktop/lda/mid_data/layerCompare.txt";
+	public static double connectThreshold=2.8;
+	public static String outputPath = "C:/Users/zouc/Desktop/lda/mid_data/layerCompare.txt";  // --> obsolete
 	public static String branchPath ="C:/Users/zouc/Desktop/lda/mid_data/";
+	public static String allTopicKeywordsPath = "C:/Users/zouc/Desktop/lda/mid_data/allTopicKeywords.txt";
 	
 	public static void main(String[] args) throws Exception {
 		
 		int topicNumberOfLDARun = 40;
-		int whichTopic = 38;
+		int whichTopic = 13;
 		
 		List<String> branch = new ArrayList<String>();		
 		KLAnalysis_KLNet.getFullBranchUp(topicNumberOfLDARun, whichTopic, branch);
 		branch = KLAnalysis_KLNet.reverse(branch);
-		KLAnalysis_KLNet.getFullBranchDown(topicNumberOfLDARun, whichTopic, branch);
-		
-		
+		KLAnalysis_KLNet.getFullBranchDown(topicNumberOfLDARun, whichTopic, branch);		
 		
 		KLAnalysis_KLNet.printAndSaveBranch(branch, topicNumberOfLDARun, whichTopic, connectThreshold);
 		
+		//get topic keywords for the topics in branch		
+		KLAnalysis_KLNet.saveAllTopicKeywordsInBranch(branch, topicNumberOfLDARun, whichTopic, connectThreshold);
 	}
+	
+	//-----------------"save all topic keywords in a specific branch" START ----------------------
+	public static void saveAllTopicKeywordsInBranch(List<String> branch, int topicNumberOfLDARun, int whichTopic, double connectThreshold) throws Exception{
+		
+		// calculat a line number in alltopicKeywords file		
+		List<Integer> lines = KLAnalysis_KLNet.getSeq(branch);
+		// save lines into stack would be great for further process
+		Stack<Integer> slines = new Stack<Integer>();
+		for (int i=lines.size()-1; i>=0;i--)
+			slines.push(lines.get(i));
+		
+		File file = new File(branchPath+"branch "+topicNumberOfLDARun+" "+whichTopic+" "+connectThreshold+" topicKeyWords.txt");
+		BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+		
+		File rfile = new File(allTopicKeywordsPath);
+		BufferedReader br = new BufferedReader(new FileReader(rfile));
+		String str = "";
+		int lineNum = 1;
+		while ( (str=br.readLine())!=null ) {
+			if (slines.peek() == lineNum) {
+				bw.write(str+"\n");
+				lineNum++;
+				slines.pop();
+			} else {
+				lineNum++;
+			}
+		}
+		br.close();
+		bw.close();
+	}
+	private static List<Integer> getSeq(List<String> branch) {
+		
+		List<Integer> seq = new ArrayList<Integer>();		
+//		branch.add("[(1 0)->(3 2)] ");
+//		branch.add("[(108 95)->(109 50)(109 83)] ");
+//		branch.add("[(109 50)->(110 21)] [(109 83)->(110 21)] ");
+		for (String  s:branch) {
+			
+			String[] arr1 = s.split("\\)");
+			
+			for (String ss:arr1) {
+				
+				if (ss.contains("(")) {
+					String[] arr2 = ss.split("\\(");
+					int seqNum = getSeqNum(arr2[1]);
+					seq.add(seqNum);
+				}
+			}
+			
+		}
+		Collections.sort(seq);
+		List<Integer> noDupSeq = new ArrayList<Integer>();
+		for (int i:seq) {
+			if (noDupSeq.size()==0 || noDupSeq.get(noDupSeq.size()-1) != i )
+				noDupSeq.add(i);
+		}
+		return noDupSeq;
+	}
+	// the formula is:  (1 0)=>line 1;  (3 2)=>line6; (m n)=> sum of 1 to (m-1), then add n+1
+	private static int getSeqNum(String str) {
+		String[] arr = str.split(" ");
+		int layer = Integer.valueOf(arr[0]);
+		int topic = Integer.valueOf(arr[1]);
+		int seqNum = 0;
+		for (int i=1;i<layer; i++) {
+			seqNum += i;
+		}
+		seqNum += topic + 1;
+		return seqNum;
+	}
+	//------------------"save all topic keywords in a specific branch" END ------------------
 	
 	public static void getFullBranchUp(int layer, int whichTopic, List<String> branch) throws Exception {
 		Hashtable<DictDistribution, List<Double>> backTraceMap = KLUtil_generateTraceMap.readBackTraceMapFromFile();
