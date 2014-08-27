90e7fe2859d0c9067adb7adc9ccba30e729e5d5a
zouc <zouc@LIB-12F14.ad.clarkson.edu>
2014-08-26 21:48:01 -0400

    8.26

--- Phase2_KL/src/phase2/Constant.java
+++ Phase2_KL/src/phase2/Constant.java
@@ -2,12 +2,14 @@ package phase2;
 
 public class Constant {
 	
-	public static int 		topicNumberOfLDARun = 200;
-	public static int 		whichTopic = 79;
+	public static int 		topicNumberOfLDARun = 40;
+	public static int 		whichTopic = 26;
 	public static double 	connectThreshold=0;
 	public static double 	fixedconnectThreshold=0;
 	public static double    connectThresholdMinus = 0.00; //initial 0.005
 		
+	/*   
+	// nancy's laptop
 	public static String branchPath ="C:/Users/nancy.quan/Desktop/lda/mid_data/";
 	public static String allTopicKeywordsPath = "C:/Users/nancy.quan/Desktop/lda/mid_data/allTopicKeywords.txt";
 	public static String autoScanConnection =   "C:/Users/nancy.quan/Desktop/lda/mid_data/";
@@ -16,5 +18,17 @@ public class Constant {
 	public static String PHIFILEPATH = "C:/Users/nancy.quan/Desktop/lda/output/casestudy/";
 	public static String TRACEMAPPATH =     "C:/Users/nancy.quan/Desktop/lda/mid_data/traceMap.txt";
 	public static String BACKTRACEMAPPATH = "C:/Users/nancy.quan/Desktop/lda/mid_data/backTraceMap.txt";	
+	*/
+	
+	
+	// public computers
+	public static String branchPath ="C:/Users/zouc/Desktop/lda/mid_data/";
+	public static String allTopicKeywordsPath = "C:/Users/zouc/Desktop/lda/mid_data/allTopicKeywords.txt";
+	public static String autoScanConnection =   "C:/Users/zouc/Desktop/lda/mid_data/";
+	
+	
+	public static String PHIFILEPATH = "C:/Users/nancy.quan/Desktop/lda/output/casestudy/";
+	public static String TRACEMAPPATH =     "C:/Users/zouc/Desktop/lda/mid_data/traceMap.txt";
+	public static String BACKTRACEMAPPATH = "C:/Users/zouc/Desktop/lda/mid_data/backTraceMap.txt";		
 
 }
